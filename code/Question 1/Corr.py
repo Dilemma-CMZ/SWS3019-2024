46ad5808735f38146929e7ba214360b048823db9
r-q <scrisqiu@hotmail.com>
2024-07-09 22:03:26 +0800

    fix daxing bad malls

--- code/Question 1/Corr.py
+++ code/Question 1/Corr.py
@@ -3,6 +3,6 @@ import pandas as pd
 if __name__ == '__main__':
     df = pd.read_csv('../../data/district.csv')
     corr = df.corr(method='pearson')
-    # corr = corr['mall_cnt']
+    corr = corr['mall_cnt']
     print(corr)
     corr.to_csv('../../data/Corr_mall.csv')
