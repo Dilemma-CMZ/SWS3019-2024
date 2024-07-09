3a45dc2a6dc290ea660c211e7902fd84e211e4d2
Krurmi33 <2397429787@qq.com>
2024-07-09 22:07:41 +0800

    update: corr related

--- code/Question 1/Corr.py
+++ code/Question 1/Corr.py
@@ -3,6 +3,7 @@ import pandas as pd
 if __name__ == '__main__':
     df = pd.read_csv('../../data/district.csv')
     corr = df.corr(method='pearson')
+    corr.to_csv('../../data/Corr.csv')
     corr = corr['mall_cnt']
     print(corr)
     corr.to_csv('../../data/Corr_mall.csv')
